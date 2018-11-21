510eb256ef177e5d5154befdf0f9e1ff29dcabf6
daheige <zhuwei313@hotmail.com>
2018-11-21 22:31:08 +0800

    修复axios post请求baseUrl问题

--- application/Controller/api/DemoController.js
+++ application/Controller/api/DemoController.js
@@ -9,21 +9,20 @@ let DemoController = {
     //采用async+await方式异步调用接口
     test: async function(ctx, next) {
         // axios调用后是一个promise
-        let r = await axios.get('http://localhost:8080/test');
+        let r = await axios.get('http://localhost:8080', '/test');
         // console.log("res: ", r);
         helper.AjaxReturn(ctx, r);
     },
     getData: async function(ctx, next) {
         // helper.get调用后是一个promise
-        let r = await helper.get('http://localhost:8080/test');
+        let r = await helper.get('http://localhost:8080', '/test');
         // console.log("res: ", r);
         helper.AjaxReturn(ctx, r);
     },
     testPost: async function(ctx, next) {
-        // ctx.body = "fefe"
         // helper.get调用后是一个promise
-        let r = await helper.post('http://localhost:8080/home/post');
-        console.log("res: ", r);
+        let r = await helper.post('http://localhost:8080', '/home/post');
+        console.log("======res=====", r);
         helper.AjaxReturn(ctx, r);
     }
 };
